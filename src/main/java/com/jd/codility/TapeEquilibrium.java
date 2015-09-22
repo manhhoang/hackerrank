6d956ddc453bc6d0ce1527804d3f26c660dbaae8
Mike Ha <hamanhhoang1102@gmail.com>
2015-09-22 08:58:16 +0800

    update

---
+++ src/main/java/com/jd/codility/TapeEquilibrium.java
@@ -0,0 +1,20 @@
+package com.jd.codility;
+
+public class TapeEquilibrium {
+  
+  public static int solution(int[] A) {
+    return -1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int[] A = new int[5];
+    A[0] = 3;
+    A[1] = 1;
+    A[2] = 2;
+    A[3] = 4;
+    A[4] = 3;
+
+    int ans = solution(A);
+    System.out.println(ans);
+  }
+}
