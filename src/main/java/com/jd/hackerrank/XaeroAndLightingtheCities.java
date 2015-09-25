3ab358ca8f37c7c1382b27d1bda48c6c5a83c78b
Mike Ha <hamanhhoang1102@gmail.com>
2015-09-25 15:51:51 +0800

    udpate

--- src/main/java/com/jd/hackerrank/XaeroAndLightingtheCities.java
+++ src/main/java/com/jd/hackerrank/XaeroAndLightingtheCities.java
@@ -1,40 +1,54 @@
 package com.jd.hackerrank;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class XaeroAndLightingtheCities {
 
-  @SuppressWarnings("resource")
+  private static boolean[] config;
+  private static List<Integer>[] g;
+  private static int[][] d;
+
+  private static void dfs(int u, int p) {
+    for (int v : g[u]) {
+      if (v != p) {
+        dfs(v, u);
+        d[0][u] += d[1][v];
+        d[1][u] += Math.min(d[0][v], d[1][v]);
+      }
+    }
+
+    if (!config[u]) {
+      ++d[1][u];
+    }
+  }
+
+  @SuppressWarnings({"resource", "unchecked"})
   public static void main(String[] args) throws Exception {
     String currentPath = new File(".").getCanonicalPath();
     String fileName = currentPath + "/src/Xaero And Lighting the Cities";
     File file = new File(fileName);
-
     Scanner sc = new Scanner(file);
     int t = sc.nextInt();
     for (int i = 0; i < t; i++) {
       int n = sc.nextInt();
-      int[] config = new int[n + 1];
-      for (int j = 1; j < n; j++) {
-        config[j] = sc.nextInt();
+      g = new ArrayList[n];
+      config = new boolean[n];
+      for (int j = 0; j < n; j++) {
+        config[j] = sc.nextInt() == 1;
+        g[j] = new ArrayList<Integer>();
       }
-      sc.nextLine();
-      boolean[] city = new boolean[n + 1];
-      int count = 0;
-      for (int k = 0; k < n - 1; k++) {
-        String[] pair = sc.nextLine().split(" ");
-        int c1 = Integer.parseInt(pair[0]);
-        int c2 = Integer.parseInt(pair[1]);
-        if (config[c1] == 1 || config[c2] == 1) {
-          //config[c1] = true;
-          //config[c2] = true;
-        } else {
-          config[c2] = 1;
-          count++;
-        }
+      for (int k = 1; k < n; k++) {
+        int u = sc.nextInt() - 1;
+        int v = sc.nextInt() - 1;
+        g[u].add(v);
+        g[v].add(u);
       }
-      System.out.println(count);
+      d = new int[2][n];
+      dfs(0, -1);
+      System.out.println(Math.min(d[0][0], d[1][0]));
     }
   }
 }
