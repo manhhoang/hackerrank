853602d4b500b84dcd75268512312409d6db4512
Mike Ha <hamanhhoang1102@gmail.com>
2015-10-02 12:37:00 +0800

    update

---
+++ src/main/java/com/jd/hackerrank/TheLongestCommonSubsequence.java
@@ -0,0 +1,19 @@
+package com.jd.hackerrank;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TheLongestCommonSubsequence {
+
+  @SuppressWarnings("resource")
+  public static void main(String[] args) throws Exception {
+    String currentPath = new File(".").getCanonicalPath();
+    String fileName = currentPath + "/src/The Longest Common Subsequence";
+    File file = new File(fileName);
+
+    Scanner sc = new Scanner(file);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    
+  }
+}
