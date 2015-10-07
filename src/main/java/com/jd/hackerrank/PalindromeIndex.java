e638d8c864e85d5be29f4e6060661aeb17d7b491
Mike Ha <hamanhhoang1102@gmail.com>
2015-10-07 15:28:39 +0800

    update

---
+++ src/main/java/com/jd/hackerrank/PalindromeIndex.java
@@ -0,0 +1,47 @@
+package com.jd.hackerrank;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class PalindromeIndex {
+
+  static boolean isPalidrom(String s, int i, int j) {
+    while (i < j) {
+      if (s.charAt(i++) != s.charAt(j--)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @SuppressWarnings("resource")
+  public static void main(String[] args) throws Exception {
+    String currentPath = new File(".").getCanonicalPath();
+    String fileName = currentPath + "/src/Palindrome Index";
+    File file = new File(fileName);
+
+    Scanner sc = new Scanner(file);
+    int T = sc.nextInt();
+    for (int t = 0; t < T; t++) {
+      String s = sc.next();
+      int i = 0;
+      int j = s.length() - 1;
+      while (i < j) {
+        if (s.charAt(i) != s.charAt(j)) {
+          if (isPalidrom(s, i, j - 1)) {
+            System.out.println(j);
+          } else {
+            System.out.println(i);
+          }
+          break;
+        }
+        i++;
+        j--;
+      }
+
+      if (i >= j) {
+        System.out.println(-1);
+      }
+    }
+  }
+}
