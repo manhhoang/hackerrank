f6cd783c6031fc7ad93b75f4c499e83ebe9fd604
Mike Ha <hamanhhoang1102@gmail.com>
2015-11-17 15:51:02 +0800

    update LCA

---
+++ src/main/java/com/jd/hackerrank/cs/LCABinaryTree.java
@@ -0,0 +1,39 @@
+package com.jd.hackerrank.cs;
+
+class Node {
+  public int data;
+  public Node right;
+  public Node left;
+
+  public Node(int data) {
+    this.data = data;
+  }
+}
+
+public class LCABinaryTree {
+
+  public static Node lca(Node root, Node a, Node b) {
+    if (root == null) {
+      return null;
+    }
+    if (root.equals(a) || root.equals(b)) {
+      // if at least one matched, no need to continue
+      // this is the LCA for this root
+      return root;
+    }
+    Node l = lca(root.left, a, b);
+    Node r = lca(root.right, a, b);
+    if (l != null && r != null) {
+      return root; // nodes are each on a seaparate branch
+    }
+    // either one node is on one branch,
+    // or none was found in any of the branches
+    return l != null ? l : r;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
