eef8cfafff640f17da563c01f9c43ccb22b99e2f
Mike <hamanhhoang1102@gmail.com>
2015-11-18 01:21:10 +0800

    update

---
+++ src/main/java/com/jd/hackerrank/Fibonaci.java
@@ -0,0 +1,24 @@
+package com.jd.hackerrank;
+
+public class Fibonaci {
+
+  private static int ans = 0;
+
+  public static void main(String[] args) throws Exception {
+    int n = 6;
+    int count = fibo(1, 2, n, 2);
+    System.out.println("count=" + count + " == ans=" + ans);
+  }
+
+  private static int fibo(int a, int b, int n, int count) {
+    int fibo = a * b;
+    count++;
+    if (count < n) {
+      int c = fibo(b, fibo, n, count);
+      count = c;
+    } else {
+      ans = fibo;
+    }
+    return count;
+  }
+}
