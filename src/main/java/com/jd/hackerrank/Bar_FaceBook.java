85f5fb6f80ff0093dc96dbfedce062d2e4632ace
Mike Ha <hamanhhoang1102@gmail.com>
2015-11-25 16:39:02 +0800

    update Bar

--- src/main/java/com/jd/hackerrank/Bar_FaceBook.java
+++ src/main/java/com/jd/hackerrank/Bar_FaceBook.java
@@ -4,36 +4,31 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeMap;
 
 public class Bar_FaceBook {
 
-  private static Map<Integer, Integer[]> map = new HashMap<Integer, Integer[]>();
+  private static Set<Integer> ans = new HashSet<Integer>();
 
-  private static void dfs(int v, int K, List<Integer> a, List<List<Integer>> ret) {
-    if (map.get(v) == null) {
-      ret.add(new ArrayList<Integer>(a));
-      Iterator<Integer> i = a.iterator();
-      int c = 0;
-      while (i.hasNext()) {
-        Integer o = i.next();
-        if (c > 1)
-          i.remove();
-        c++;
-      }
-    } else {
-      for (int w : map.get(v)) {
-        a.add(w);
-        dfs(w, K, a, ret);
-      }
+  private static void process(int K, List<Integer> output) {
+    Collections.sort(output);
+    ans.add(output.get(K - 2));
+  }
+
+  private static void enumarate(int d, int[][] a, int K, List<Integer> output) {
+    if (d == a.length) {
+      process(K, output);
+      return;
     }
+    for (int i = 0; i < a[d].length; i++) {
+      output.add(a[d][i]);
+      enumarate(d + 1, a, K, output);
+      output.remove(output.size() - 1);
+    }
+    return;
   }
 
   public static void main(String[] args) throws IOException {
@@ -46,33 +41,16 @@ public class Bar_FaceBook {
     for (int t = 0; t < T; t++) {
       int N = sc.nextInt(); // number of person
       int K = sc.nextInt(); // Kth larget number
-      Map<Integer, Integer[]> treeMap = new TreeMap<Integer, Integer[]>();
+      int[][] a = new int[3][];
       for (int i = 0; i < N; i++) {
         int A = sc.nextInt(); // number of list for each person
-        Integer[] a = new Integer[A]; // list number of each
+        a[i] = new int[A]; // list number of each
         for (int j = 0; j < A; j++) {
-          a[j] = sc.nextInt(); // element of list
-        }
-        treeMap.put(i, a);
-      }
-      for (int i = 0; i < N - 1; i++) {
-        Integer[] values = treeMap.get(i);
-        for (int j = 0; j < values.length; j++) {
-          map.put(values[j], treeMap.get(i + 1));
+          a[i][j] = sc.nextInt(); // element of list
         }
       }
-      Integer[] firstList = treeMap.get(0);
-      List<List<Integer>> ret = new ArrayList<List<Integer>>();
-      for (int v : firstList) {
-        List<Integer> a = new ArrayList<Integer>();
-        a.add(v);
-        dfs(v, K, a, ret);
-      }
-      Set<Integer> ans = new HashSet<Integer>();
-      for (List<Integer> list : ret) {
-        Collections.sort(list);
-        ans.add(list.get(K - 1));
-      }
+      List<Integer> output = new ArrayList<>();
+      enumarate(0, a, K, output);
       System.out.println(ans.size());
     }
   }
--- src/main/java/com/jd/hackerrank/cs/CombinationsMultipleArray.java
+++ src/main/java/com/jd/hackerrank/cs/CombinationsMultipleArray.java
@@ -5,53 +5,58 @@ import java.util.List;
 
 public class CombinationsMultipleArray {
 
-  private static void printSolve(int d, String[][] a, List<String> output) {
+  private static void process(List<String> output) {
+    System.out.println(output.toString());
+    // output.stream().forEach(x -> System.out.print(x));
+  }
+
+  private static void enumarate(int d, String[][] a, List<String> output) {
     if (d == a.length) {
-      System.out.println(output.toString());
-      // output.stream().forEach(x -> System.out.print(x));
+      process(output);
       return;
     }
     for (int i = 0; i < a[d].length; i++) {
       output.add(a[d][i]);
-      printSolve(d + 1, a, output);
+      enumarate(d + 1, a, output);
       output.remove(output.size() - 1);
     }
     return;
   }
 
-  private static void printSolve(int d, String s, String[][] a) {
+  private static void enumarate(int d, String s, String[][] a) {
     if (d == a.length) {
       System.out.println(s.substring(0, s.length() - 1));
       return;
     }
     for (int i = 0; i < a[d].length; i++) {
-      printSolve(d + 1, s + a[d][i] + " ", a);
+      enumarate(d + 1, s + a[d][i] + " ", a);
     }
     return;
   }
 
-  private static int countSolve(int d, String s, String[][] a) {
+  private static int countEnumarate(int d, String s, String[][] a) {
     int count = 0;
     if (d == a.length) {
       return 1;
     }
     for (int i = 0; i < a[d].length; i++) {
-      int c = countSolve(d + 1, s + a[d][i] + " ", a);
+      int c = countEnumarate(d + 1, s + a[d][i] + " ", a);
       count += c;
     }
     return count;
   }
 
   public static void main(String[] args) {
-    String[][] a =
+    String[][] a = { {"0", "1", "2"}, {"0", "1", "2"}, {"0", "1", "2"}};
+    List<String> output = new ArrayList<>();
+    enumarate(0, a, output);
+    System.out.println();
+
+    String[][] b =
         {
             {"A1", "A2"}, {"B1", "B2", "B3"}, {"C1", "C2", "C3", "C4"}, {"D1", "D2", "D3", "D4"},
             {"E1", "E2"}};
-
-    printSolve(0, "", a);
-    System.out.println("Total combination: " + countSolve(0, "", a));
-
-    List<String> output = new ArrayList<>();
-    printSolve(0, a, output);
+    enumarate(0, "", b);
+    System.out.println("Total combination: " + countEnumarate(0, "", b));
   }
 }
