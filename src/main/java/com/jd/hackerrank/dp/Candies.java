cd35f080d972d59d8a7dd2831c5184160a21aa85
Mike <hamanhhoang1102@gmail.com>
2015-12-03 23:28:06 +0800

    update

--- src/main/java/com/jd/hackerrank/dp/Candies.java
+++ src/main/java/com/jd/hackerrank/dp/Candies.java
@@ -1,5 +1,7 @@
 package com.jd.hackerrank.dp;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 import junit.framework.Assert;
@@ -21,17 +23,17 @@ public class Candies {
     }
 
     for (int i = 0; i < N - 1; i++) {
-      if (a[i] < a[i + 1]) {
+      if (a[i] < a[i + 1] && dp[i] >= dp[i + 1]) {
         dp[i + 1] = dp[i] + 1;
-      } else if (a[i] == a[i + 1]) {
-        if (dp[i + 1] != 1 && dp[i] < dp[i + 1]) {
-          dp[i + 1] = dp[i + 1] + 1;
-        } else if (dp[i + 1] != 1 && dp[i] >= dp[i + 1]) {
-          dp[i + 1] = dp[i] + 1;
-        }
       }
     }
     return Arrays.stream(dp).sum();
+  }
+
+  public static void main(String[] args) throws IOException {
+    String currentPath = new File(".").getCanonicalPath();
+    String fileName = currentPath + "/src/Candies";
+    File file = new File(fileName);
   }
 
   @Test
@@ -43,13 +45,61 @@ public class Candies {
   @Test
   public void test2() {
     int[] a = new int[] {1, 3, 3, 2};
-    Assert.assertEquals(7, Candies.solve(a));
+    Assert.assertEquals(6, Candies.solve(a));
   }
 
   @Test
   public void test3() {
     int[] a = new int[] {2, 4, 2, 6, 1, 7, 8, 9, 2, 1};
     Assert.assertEquals(19, Candies.solve(a));
+  }
+
+  @Test
+  public void test4() {
+    int[] a = new int[] {2, 2, 2, 2};
+    Assert.assertEquals(4, Candies.solve(a));
+  }
+
+  @Test
+  public void test5() {
+    int[] a = new int[] {1, 2, 3, 4};
+    Assert.assertEquals(10, Candies.solve(a));
+  }
+
+  @Test
+  public void test6() {
+    int[] a = new int[] {4, 3, 2, 1};
+    Assert.assertEquals(10, Candies.solve(a));
+  }
+
+  @Test
+  public void test7() {
+    int[] a = new int[] {2, 1, 2, 1};
+    Assert.assertEquals(6, Candies.solve(a));
+  }
+
+  @Test
+  public void test8() {
+    int[] a = new int[] {1, 2, 1, 2};
+    Assert.assertEquals(6, Candies.solve(a));
+  }
+
+  @Test
+  public void test9() {
+    int[] a = new int[] {1, 2};
+    Assert.assertEquals(3, Candies.solve(a));
+  }
+
+  @Test
+  public void test10() {
+    int[] a = new int[] {1, 1};
+    Assert.assertEquals(2, Candies.solve(a));
+  }
+
+  @Test
+  public void test11() {
+    int[] a = new int[] {1, 2, 4, 3, 2, 1};
+    Assert.assertEquals(13, Candies.solve(a));
   }
 
 }
