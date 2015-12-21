0b7fa10305a37f9fa9e01d76687ccabcecce7521
Mike Ha <hamanhhoang1102@gmail.com>
2015-12-21 18:27:11 +0800

    update

--- src/main/java/com/jd/hackerrank/greedy/PriyankaandToys.java
+++ src/main/java/com/jd/hackerrank/greedy/PriyankaandToys.java
@@ -2,6 +2,7 @@ package com.jd.hackerrank.greedy;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PriyankaandToys {
@@ -11,7 +12,30 @@ public class PriyankaandToys {
     String fileName = currentPath + "/src/Priyanka and Toys";
     File file = new File(fileName);
     Scanner sc = new Scanner(file);
-
+    int N = sc.nextInt();
+    int[] a = new int[N];
+    for (int i = 0; i < N; i++) {
+      a[i] = sc.nextInt();
+    }
+    Arrays.sort(a);
+    int c = 0;
+    for (int i = 0; i < N;) {
+      int j = i;
+      c++;
+      while (j < N) {
+        if (a[j] <= a[i] + 4) {
+          j++;
+        } else {
+          break;
+        }
+      }
+      if (i == j) {
+        i++;
+      } else {
+        i = j;
+      }
+    }
+    System.out.println(c);
   }
 
 }
