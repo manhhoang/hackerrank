d4951d1d02fe97f2333bca99e479f21524948617
Mike Ha <hamanhhoang1102@gmail.com>
2015-12-24 12:10:10 +0800

    udpate

--- src/main/java/com/jd/hackerrank/tree/LCA_CommonManager.java
+++ src/main/java/com/jd/hackerrank/tree/LCA_CommonManager.java
@@ -4,29 +4,29 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-class NodeManager {
-  public String name;
-  public NodeManager right;
-  public NodeManager left;
-
-  public NodeManager(String name) {
-    this.name = name;
-  }
-
-  public void addChild(NodeManager node) {
-    if (this.left == null)
-      this.left = node;
-    else
-      this.right = node;
-  }
-}
-
 public class LCA_CommonManager {
 
-  private static NodeManager addNode(NodeManager node, String parentName, String childName) {
-    NodeManager childNode = new NodeManager(childName);
+  static class Node {
+    public String name;
+    public Node right;
+    public Node left;
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public void addChild(Node node) {
+      if (this.left == null)
+        this.left = node;
+      else
+        this.right = node;
+    }
+  }
+
+  private static Node addNode(Node node, String parentName, String childName) {
+    Node childNode = new Node(childName);
     if (node == null) {
-      node = new NodeManager(parentName);
+      node = new Node(parentName);
       node.addChild(childNode);
       return node;
     }
@@ -45,7 +45,7 @@ public class LCA_CommonManager {
     return node;
   }
 
-  private static NodeManager lca(NodeManager root, NodeManager a, NodeManager b) {
+  private static Node lca(Node root, Node a, Node b) {
     if (root == null) {
       return null;
     }
@@ -56,8 +56,8 @@ public class LCA_CommonManager {
       return root;
     }
 
-    NodeManager l = lca(root.left, a, b);
-    NodeManager r = lca(root.right, a, b);
+    Node l = lca(root.left, a, b);
+    Node r = lca(root.right, a, b);
 
     if (l != null && r != null) {
       return root; // nodes are each on a separate branch
@@ -76,9 +76,9 @@ public class LCA_CommonManager {
 
     Scanner sc = new Scanner(file);
     sc.nextLine();
-    NodeManager a = new NodeManager(sc.nextLine());
-    NodeManager b = new NodeManager(sc.nextLine());
-    NodeManager root = null;
+    Node a = new Node(sc.nextLine());
+    Node b = new Node(sc.nextLine());
+    Node root = null;
     while (sc.hasNext()) {
       String str = sc.nextLine();
       if (str.equals("")) {
@@ -90,7 +90,7 @@ public class LCA_CommonManager {
       root = addNode(root, parentName, childName);
     }
 
-    NodeManager ans = lca(root, a, b);
+    Node ans = lca(root, a, b);
     System.out.println(ans.name);
   }
 }
