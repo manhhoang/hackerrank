b8c68f5f9d2c48eaf46fec7aa8277a718a40dbe4
Mike <hamanhhoang1102@gmail.com>
2016-01-03 23:12:14 +0800

    update

---
+++ src/main/java/com/jd/codility/PassingCars.java
@@ -0,0 +1,30 @@
+package com.jd.codility;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PassingCars {
+
+  public int solution(int[] A) {
+    int numOfZero = 0;
+    int ans = 0;
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] == 0) {
+        numOfZero++;
+      } else {
+        ans += numOfZero;
+      }
+      if (ans > 1000000000) {
+        return -1;
+      }
+    }
+    return ans;
+  }
+
+  @Test
+  public void test1() {
+    int[] A = new int[] {0, 1, 0, 1, 1};
+    Assert.assertEquals(5, solution(A));
+  }
+
+}
