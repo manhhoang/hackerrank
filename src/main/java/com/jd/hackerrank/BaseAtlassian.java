b8a2eece7487e317859d725123c1cd2a7bb6abcb
Mike <hamanhhoang1102@gmail.com>
2016-01-20 01:43:48 +0800

    update

---
+++ src/main/java/com/jd/hackerrank/BaseAtlassian.java
@@ -0,0 +1,42 @@
+package com.jd.hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class BaseAtlassian {
+
+  private static String convertBase7(int number) {
+    return Integer.toString(number, 7);
+  }
+
+  public static String solution(int n) {
+    Map<String, String> map = new HashMap<>();
+    map.put("0", "0");
+    map.put("1", "a");
+    map.put("2", "t");
+    map.put("3", "l");
+    map.put("4", "s");
+    map.put("5", "i");
+    map.put("6", "n");
+    String base7 = convertBase7(n);
+    String ans = "";
+    for (int i = 0; i < base7.length(); i++) {
+      ans += map.get(String.valueOf(base7.charAt(i)));
+    }
+    return ans;
+  }
+
+  @Test
+  public void test1() {
+    Assert.assertEquals("a0", solution(7));
+  }
+
+  @Test
+  public void test2() {
+    Assert.assertEquals("atlassian", solution(7792875));
+  }
+}
