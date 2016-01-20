ed4b2dca88ab4ca2a2dc60e34a7bf7f84deddd05
Mike Ha <hamanhhoang1102@gmail.com>
2016-01-20 09:57:49 +0800

    update

---
+++ src/main/java/com/jd/hackerrank/linked_list/FindSubLinkedNode.java
@@ -0,0 +1,87 @@
+package com.jd.hackerrank.linked_list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindSubLinkedNode {
+
+  static class LinkedList {
+    int val;
+    LinkedList next;
+
+    LinkedList(int node_value) {
+      val = node_value;
+      next = null;
+    }
+  };
+
+  public static LinkedList insert(LinkedList head, int val) {
+    if (head == null) {
+      head = new LinkedList(val);
+    } else {
+      LinkedList end = head;
+      while (end.next != null) {
+        end = end.next;
+      }
+      LinkedList node = new LinkedList(val);
+      end.next = node;
+    }
+    return head;
+  }
+
+  static int find(LinkedList list, LinkedList sublist) {
+  }
+
+  public static int solution(int[] arr, int[] subarr) {
+    LinkedList list = null;
+    for (int i = 0; i < arr.length; i++) {
+      list = insert(list, arr[i]);
+    }
+
+    LinkedList sublist = null;
+    for (int i = 0; i < subarr.length; i++) {
+      sublist = insert(sublist, subarr[i]);
+    }
+    return find(list, sublist);
+  }
+
+  @Test
+  public void test1() {
+    Assert.assertEquals(1, solution(new int[] {1, 2, 3, 4, 5}, new int[] {2, 3, 4}));
+  }
+
+  @Test
+  public void test2() {
+    Assert.assertEquals(-1, solution(new int[] {1, 2, 3, 4, 5}, new int[] {2, 5, 4}));
+  }
+
+  @Test
+  public void test3() {
+    Assert.assertEquals(1, solution(new int[] {1, 2, 3, 4, 5}, new int[] {4, 5}));
+  }
+
+  @Test
+  public void test4() {
+    Assert.assertEquals(1, solution(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2}));
+  }
+
+  @Test
+  public void test5() {
+    Assert.assertEquals(1, solution(new int[] {1, 2, 3, 4, 5}, new int[] {2}));
+  }
+
+  @Test
+  public void test6() {
+    Assert.assertEquals(-1, solution(new int[] {1, 2, 3}, new int[] {1, 2, 3, 4}));
+  }
+
+  @Test
+  public void test7() {
+    Assert.assertEquals(-1, solution(new int[] {1, 2, 3}, new int[] {1, 2, 3, 4, 5}));
+  }
+
+  @Test
+  public void test8() {
+    Assert.assertEquals(1, solution(new int[] {2, 3, 2, 4, 5}, new int[] {2, 4}));
+  }
+}
