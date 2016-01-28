6484bb21572f2691f863cf6ca7fd1a72b7f8c045
Mike Ha <hamanhhoang1102@gmail.com>
2016-01-28 09:35:28 +0800

    update

---
+++ src/main/java/com/jd/codility/arrays/CyclicRotation.java
@@ -0,0 +1,22 @@
+package com.jd.codility.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CyclicRotation {
+
+  public static int[] solution(int A[], int K) {
+    int len = A.length;
+    int[] ans = new int[len];
+    for (int i = 0; i < len; i++) {
+      ans[(i + K) % len] = A[i];
+    }
+    return ans;
+  }
+
+  @Test
+  public void test1() {
+    Assert.assertArrayEquals(new int[] {4, 1, 2, 3}, solution(new int[] {1, 2, 3, 4}, 1));
+  }
+
+}
